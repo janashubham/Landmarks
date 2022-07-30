77fc8ff8999d589f2b8f149b4ffc3e72b33c67b2
- shubham <infuser-plays-0b@icloud.com>
2022-07-30 22:07:10 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Shubham Jana on 30/07/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
